5f86bb65cd0f9fc9813beb5957e73354964444bc
Vasyl <vasyl.laba@rubygarage.org>
2022-08-02 23:47:27 +0300

    Initial commit

---
+++ db/migrate/20220802200204_create_books.rb
@@ -0,0 +1,12 @@
+class CreateBooks < ActiveRecord::Migration[7.0]
+  def change
+    create_table :books do |t|
+      t.string :photo
+      t.string :name
+      t.string :author
+      t.integer :price
+
+      t.timestamps
+    end
+  end
+end
