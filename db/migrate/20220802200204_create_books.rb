fbcc38c8a43e848807fec6d5bef9f284d7b03dd7
Vasyl <vasyl.laba@rubygarage.org>
2022-08-07 10:45:43 +0300

    Change db

--- db/migrate/20220802200204_create_books.rb
+++ db/migrate/20220802200204_create_books.rb
@@ -2,9 +2,9 @@ class CreateBooks < ActiveRecord::Migration[7.0]
   def change
     create_table :books do |t|
       t.string :photo
-      t.string :name
-      t.string :author
-      t.integer :price
+      t.string :name, null: false
+      t.string :author, null: false
+      t.integer :price, null: false
 
       t.timestamps
     end
