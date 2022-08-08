c9c3020f492a618918ff94da9531cd8d054ded2a
Vasyl <vasyl.laba@rubygarage.org>
2022-08-08 13:18:57 +0300

    create category field in book table

---
+++ db/migrate/20220808100823_add_columnc_to_books.rb
@@ -0,0 +1,5 @@
+class AddColumncToBooks < ActiveRecord::Migration[7.0]
+  def change
+    add_column :books, :category, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_02_200204) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_08_100823) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_02_200204) do
     t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "category"
   end
 
 end
