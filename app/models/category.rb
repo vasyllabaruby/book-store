c7f7db8294b5c7d66e51ddce196da34e2cc84771
Vasyl <vasyl.laba@rubygarage.org>
2022-08-10 17:31:55 +0300

    Show categories with books count

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,11 @@
 class Category < ApplicationRecord
   has_many :books
+
+  def books_count(id)
+    Book.where(category_id: id).count
+  end
+
+  def books_countq(id)
+    count = Book.where(category_id: id).count
+  end
 end
