98f65b9676321eae49431634fe9a892e079857c8
Vasyl <vasyl.laba@rubygarage.org>
2022-08-11 12:27:24 +0300

    Show books by categories

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,18 @@
 class BooksController < ApplicationController
+
   def home
     @books = Book.all
   end
+  
   def index
-    @books = Book.all
     @categories = Category.all
+    if params[:category].present?
+      @books = Book.where(category: params[:category])
+    else
+      @books = Book.all
+    end
   end
+
   def show
     @book = Book.find(params[:id])
   end
