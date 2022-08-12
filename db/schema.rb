6d5aafe805214dff8cc96d7d2e860878be2e9a73
Vasyl <vasyl.laba@rubygarage.org>
2022-08-12 10:50:00 +0300

    Add user and cart tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_10_123236) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_12_071331) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,8 +36,22 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_10_123236) do
     t.index ["category_id"], name: "index_books_on_category_id"
   end
 
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "categories", force: :cascade do |t|
     t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "email", null: false
+    t.text "password", null: false
+    t.boolean "admin", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
