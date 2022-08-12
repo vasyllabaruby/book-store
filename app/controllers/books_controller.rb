cba37accffa05b11d82cc3d600b94896c53790fd
Vasyl <vasyl.laba@rubygarage.org>
2022-08-12 12:42:55 +0300

    Create home controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,3 @@
 class BooksController < ApplicationController
-  def home
-    @books = Book.all
-  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root "books#home"
+  root "home#index"
 
+  resources :home, only: [:index]
   resources :books
 end
