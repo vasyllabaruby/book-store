5e8d162160050c73daf1348db84bccdca6e2fe0b
Vasyl <vasyl.laba@rubygarage.org>
2022-08-22 18:36:59 +0300

    Gem=nerate fake book data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+50.times do
+  name = Faker::Book.title
+  price = Faker::Bank.account_number(digits: 3)
+  category = Category.find_by id: 4
+  author = Author.find_by id: 2
+  description = Faker::Books::Lovecraft.paragraph(sentence_count: 5, random_sentences_to_add: 3)
+  year = Faker::Bank.account_number(digits: 4) #=> 6738
+  Book.create name: name, author: author, price: price, category: category, description: description, year_of_publication: year
+end
