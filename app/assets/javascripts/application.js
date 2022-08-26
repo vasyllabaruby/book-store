5b68a5431d4c9704632137e1b019dbcca8673838
Vasyl <vasyl.laba@rubygarage.org>
2022-08-26 12:22:21 +0300

    Fix js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,19 @@
 //= require jquery
 //= require bootstrap
+
+$(document).ready(function() {
+    alert(1)
+}
+
+console.log("Hello ")
+$('[class^="read-more"]').click(function(element) {
+  element.preventDefault()
+  $(`.truncated-paragraph`).hide()
+  $(`.normal-paragraph`).show()
+})
+
+$('[class^="read-less"]').click(function(element) {
+  element.preventDefault()
+  $(`.normal-paragraph`).hide()
+  $(`.truncated-paragraph`).show()
+})
