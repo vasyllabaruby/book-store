0b3d1922cbf08c79b208e4781e3712364dd21c6e
Vasyl <vasyl.laba@rubygarage.org>
2022-08-29 13:31:21 +0300

    Pagination per page without css

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,12 +1,13 @@
 class BooksController < ApplicationController
+  include Pagy::Backend
 
   def index
     @categories = Category.all
     if params[:category].present?
-      @books = Book.search(params[:category])
+      @pagy, @books = pagy(Book.search(params[:category]))
       @curent_category_name = Category.find_by(id: params[:category]).name
     else
-      @books = Book.all
+      @pagy, @books = pagy(Book.all)
       @curent_category_name = "All"
     end
   end
